30aae0b0a23505b53d66d3533c99ac6f2c0c97ee
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-01-10 20:35:12 +0530

    Create splitarray.java

---
+++ splitarray.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+
+        // find search space
+        for (int num : nums) {
+            start = Math.max(start, num); // max element
+            end += num;                  // sum of array
+        }
+
+        // binary search
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            int sum = 0;
+            int pieces = 1;
+
+            for (int num : nums) {
+                if (sum + num > mid) {
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > k) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return start;
+    }
+}
