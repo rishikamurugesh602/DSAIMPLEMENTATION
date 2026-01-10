e2d29b43985d934d5d5a4c4d4c035d82ee86ff93
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-01-10 23:45:26 +0530

    Create matrixsearch.java

---
+++ matrixsearch.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class rowcolmatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 40},
+            {28, 29, 37, 49}
+        };
+
+        System.out.println(Arrays.toString(search(arr, 37)));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix[0].length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
