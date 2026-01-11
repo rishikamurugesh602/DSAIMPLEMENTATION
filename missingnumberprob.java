6bec8831e1343a05dc240e15c63d028b3c96e192
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-01-11 19:13:51 +0530

    Create missingnumberprob.java

---
+++ missingnumberprob.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        cyclic(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+}
